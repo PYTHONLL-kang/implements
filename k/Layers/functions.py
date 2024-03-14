0133657d12dd1315cc8f5eea874191ecbca36e8f
PYTHONLL-kang <dlrkd0987@gmail.com>
2024-03-14 11:38:51 +0900

    feat : rnn

--- k/Layers/functions.py
+++ k/Layers/functions.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import k.Layers.base_functions as Layers
+import k.Activations.functions as Activations
 
 class Dense(Layers.Base):
     def __init__(self, output_shape, **kwargs):
@@ -11,18 +12,22 @@ class Dense(Layers.Base):
         self.weight_shape = [self.input_shape, self.output_shape]
     
     def forward(self, data):
-        matmul_output = tf.matmul(data, self.weight)
-        return tf.math.add(matmul_output, self.bias)
+        output = tf.matmul(data, self.weight)
+
+        if self.use_bias:
+            output = tf.math.add(output, self.bias)
+
+        return output
 
     def backward(self, gradients, inputs, outputs):
         gradients = self.process_gradients(gradients)
 
         weight_gradients = tf.matmul(tf.transpose(inputs), gradients)
-        bias_gradients = tf.math.reduce_sum(gradients, axis=0, keepdims=True)
+        self.weight.assign(self.weight_optimizer.update(self.weight, weight_gradients))
 
-        param = self.optimizer.update([self.weight, self.bias], [weight_gradients, bias_gradients])
-        self.weight.assign(param[0])
-        self.bias.assign(param[1])
+        if self.use_bias:
+            bias_gradients = tf.math.reduce_sum(gradients, axis=0, keepdims=True)
+            self.bias.assign(self.bias_optimizer.update(self.bias, bias_gradients))
 
         return tf.matmul(gradients, tf.transpose(self.weight))
 
@@ -47,20 +52,23 @@ class Convolution2D(Layers.Base, Layers.Image):
         self.col_weight = tf.reshape(self.weight, (tf.reduce_prod(self.weight_shape) // self.filters, -1))
 
         col_output = tf.matmul(self.col, self.col_weight)
-        convolution_output = tf.reshape(col_output, (num, out_h, out_w, self.filters))
+        output = tf.reshape(col_output, (num, out_h, out_w, self.filters))
 
-        return tf.math.add(convolution_output, self.bias)
+        if self.use_bias:
+            output = tf.math.add(output, self.bias)
+
+        return output
 
     def backward(self, gradients, inputs, outputs):
         gradients = self.process_gradients(gradients)
         col_gradients = tf.reshape(gradients, (-1, self.filters))
 
         weight_gradients = tf.reshape(tf.matmul(tf.transpose(self.col), col_gradients), self.weight_shape)
-        bias_gradients = tf.math.reduce_sum(col_gradients, axis=0, keepdims=True)
+        self.weight.assign(self.weight_optimizer.update(self.weight, weight_gradients))
 
-        param = self.optimizer.update([self.weight, self.bias], [weight_gradients, bias_gradients])
-        self.weight.assign(param[0])
-        self.bias.assign(param[1])
+        if self.use_bias:
+            bias_gradients = tf.math.reduce_sum(gradients, axis=0, keepdims=True)
+            self.bias.assign(self.bias_optimizer.update(self.bias, bias_gradients))
 
         col_weight_gradients = tf.matmul(col_gradients, tf.transpose(self.col_weight))
 
@@ -123,3 +131,97 @@ class Flatten:
 
     def backward(self, gradients, inputs, outputs):
         return tf.reshape(tf.math.multiply(outputs, gradients), inputs.shape)
+
+class Recurrent_cell:
+    def __init__(self, input_shape, output_shape, **kwargs):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.hidden_shape = self.output_shape
+
+        self.ih_layer = Dense(input_shape=self.input_shape, output_shape=self.hidden_shape, use_bias=False, **kwargs)
+        self.hh_layer = Dense(input_shape=self.hidden_shape, output_shape=self.hidden_shape, use_bias=True, **kwargs)
+        self.ho_layer = Dense(input_shape=self.hidden_shape, output_shape=self.output_shape, use_bias=True, **kwargs)
+
+        self.activations = Activations.Tanh()
+
+    def set_vars(self, optimizer):
+        self.ih_layer.set_shape()
+        self.hh_layer.set_shape()
+        self.ho_layer.set_shape()
+
+        self.ih_layer.set_vars(optimizer)
+        self.hh_layer.set_vars(optimizer)
+        self.ho_layer.set_vars(optimizer)
+
+    def forward(self, inputs, hiddens):
+        a = self.ih_layer.forward(inputs) + self.hh_layer.forward(hiddens)
+        h = self.activations.forward(a)
+        o = self.ho_layer.forward(h)
+
+        return h, o
+
+    def backward(self, gradients, inputs, pre_hiddens, cur_hiddens, outputs):
+        ho_gradients = self.ho_layer.backward(gradients, cur_hiddens, outputs)
+        h_gradients = self.activations.backward(ho_gradients, inputs, outputs)
+
+        input_graidents = self.ih_layer.backward(h_gradients, inputs, pre_hiddens)
+        hidden_graidents = self.hh_layer.backward(h_gradients, pre_hiddens, cur_hiddens)
+
+        return input_graidents, hidden_graidents
+
+class Recurrent_network:
+    def __init__(self, output_shape, input_shape, **kwargs):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.timestep = self.input_shape[0]
+
+        self.return_sequences = kwargs.get('return_sequences', False)
+        self.return_states = kwargs.get('return_states', False)
+
+        self.network = []
+        self.hidden_shape = output_shape
+        self.network.append(Recurrent_cell(input_shape[1], self.hidden_shape))
+        for i in range(self.timestep-2):
+            self.network.append(Recurrent_cell(input_shape[1], self.hidden_shape))
+        self.network.append(Recurrent_cell(input_shape[1], output_shape))
+
+    def set_shape(self):
+        if self.return_sequences:
+            self.output_shape = (self.timestep, self.output_shape)
+        else:
+            self.output_shape = self.output_shape
+
+    def set_vars(self, optimizer):
+        for i in range(self.timestep):
+            self.network[i].set_vars(optimizer)
+    
+    def forward(self, data):
+        o = []
+        h = [tf.zeros((data.shape[0], self.hidden_shape))]
+        for i in range(self.timestep):
+            oi, hi = self.network[i].forward(data[:,i,:], h[i])
+
+            o.append(oi)
+            h.append(hi)
+
+        self.hiddens = h
+        o = tf.Variable(o)
+
+        if not self.return_sequences:
+            o = o[-1]
+
+        if self.return_states:
+            return o, h
+
+        return o
+
+    def backward(self, gradients, inputs, outputs):
+        for i in reversed(range(self.timestep)):
+            if self.return_sequences:
+                o = outputs[:,i,:]
+            else:
+                o = outputs
+
+            input_gradients, gradients = self.network[i].backward(gradients, inputs[:,i,:], self.hiddens[i-1], self.hiddens[i], o)
+        
+        return input_gradients
